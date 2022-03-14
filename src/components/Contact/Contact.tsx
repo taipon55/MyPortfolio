a03c351e71af9be1eb69668ed05c23e6bde21174
白方大成 <shirakatataisei@shirakatataiseinoMacBook-Pro.local>
2022-03-15 00:27:55 +0900

    contactページ追加

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,10 +4,16 @@ import Button from "../Button/Button";
 export const Contact = () => {
   return (
     <>
-      <h1>Contact</h1>
-      <p>お問い合わせはこちらからお願いします。</p>
-      {/* ボタンを表示 */}
-      <Button />
+      <div className="contact-photo">
+        <div className="bg_dark"></div>
+
+        <div className="contact-main">
+          <h1 className="contact-title">Contact</h1>
+          <p className="contact-text">お問い合わせはこちらからお願いします。</p>
+          {/* ボタンを表示 */}
+          <Button />
+        </div>
+      </div>
     </>
   );
 };
