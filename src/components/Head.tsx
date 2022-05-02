de92e19a6f4564c86e5fdc0681a45bb27d743f58
taipon55 <git config --global user.email xxxxxx@gmail.com>
2022-05-02 14:03:44 +0900

    ハンバーガーメニュー追加

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,29 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as Scroll } from "react-scroll";
 
 function Head() {
+  const [active, setActive] = useState(false);
+
+  const classToggle = () => {
+    setActive(!active);
+  };
+
   return (
     <div>
       <header>
         <h1>My Portfolio</h1>
+        <div className={active === true ? "navArea" : "open"}>
+          <nav>
+            <div className="inner">
+              <ul>
+                <li>
+                  <Scroll to="content-title" smooth={true}>
+                    ABOUT
+                  </Scroll>
+                </li>
+                <li>
+                  <Scroll to="portfolio" smooth={true}>
+                    PORTFOLIO
+                  </Scroll>
+                </li>
+                <li>
+                  <Scroll to="contact-photo" smooth={true}>
+                    CONTACT
+                  </Scroll>
+                </li>
+              </ul>
+            </div>
+          </nav>
 
-        <ul>
-          <li>
-            <Scroll to="content-title" smooth={true}>
-              ABOUT
-            </Scroll>
-          </li>
-          <li>
-            <Scroll to="portfolio" smooth={true}>
-              PORTFOLIO
-            </Scroll>
-          </li>
-          <li>
-            <Scroll to="contact-photo" smooth={true}>
-              CONTACT
-            </Scroll>
-          </li>
-        </ul>
+          <div id="toggle-btn" onClick={classToggle}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          <div className="mask" onClick={classToggle}></div>
+        </div>
       </header>
     </div>
   );
