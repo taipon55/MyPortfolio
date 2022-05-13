6ed6f46ce55df04936e8f47f842b4559744a4aad
taipon55 <git config --global user.email xxxxxx@gmail.com>
2022-05-14 02:18:34 +0900

    リンクの修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Button = () => {
-  let navigate = useNavigate();
   return (
     <>
-      <div
-        className="btn-position"
-        onClick={() => {
-          navigate("/form");
-        }}
-      >
-        <a href="" className="btn btn-flat">
+      <div className="btn-position">
+        <Link to="/form" className="btn btn-flat">
           <span>CONTACT</span>
-        </a>
+        </Link>
       </div>
     </>
   );
